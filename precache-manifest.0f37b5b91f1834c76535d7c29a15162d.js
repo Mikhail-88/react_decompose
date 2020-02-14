9356ecefff41f228697a3dadb05ba0e7f2a3cb21
Mikhail <mikhail.sapuzhak@gmail.com>
2020-02-14 20:13:43 +0200

    Updates

---
+++ precache-manifest.0f37b5b91f1834c76535d7c29a15162d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c392c2d3122ca5db4d8f819c560060ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c6d4b5d15e2e828fd87e",
+    "url": "./static/css/main.d3cd2504.chunk.css"
+  },
+  {
+    "revision": "79d6f7d5a6832a173977",
+    "url": "./static/js/2.ce3d7f05.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.ce3d7f05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6d4b5d15e2e828fd87e",
+    "url": "./static/js/main.be399397.chunk.js"
+  },
+  {
+    "revision": "e67b79006193623cc7c8",
+    "url": "./static/js/runtime-main.a19c704a.js"
+  }
+]);
